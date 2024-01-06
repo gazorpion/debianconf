8ca09a4e6ada73eca688652ff72ab1a027664440
gazorpion <56800098+gazorpion@users.noreply.github.com>
2024-01-06 17:38:43 +0000

    Update conf.sh

--- conf.sh
+++ conf.sh
@@ -1,2 +1,2 @@
 sudo apt install wget git screenfetch flatpak macchanger && cd ~/Downloads/ && wget https://gitlab.com/volian/nala/uploads/4509f695f7a531282e0d8aba71eb3134/nala_0.13.0_all.deb && sudo dpkg -i nala_0.13.0_all.deb &&
-sudo apt-get install -f
+sudo apt-get install -f && sudo apt --fix-broken-install && cd ~/Downloads && sudo dpkg -i nala_0.13.0_all.deb
